734fb46ddb0652806960f5530b40804b7476aa11
Mina Qolizadeh <mina.qolizadeh@hotmail.com>
2020-04-05 12:26:15 +0430

    add

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,37 @@
-import React , {useState, useRef} from 'react'
+import React , {useState, useRef , useEffect} from 'react'
 import TodoList from './TodoList'
- import uuidv4 from 'uuid/v4'
+import uuidv4 from 'uuid/v4'
+import '../index.css';
+import {Button
+,Input,
+InputGroup,
+Spinner,
+Progress,
+ButtonGroup
+} from 'reactstrap'
+const LOCAL_STOREG_KEY = 'todoApp.todos'
+
 function App() {
-    const [todos , SetTodos] = useState([{ id: 1 , name: 'todo 1' , complete: false }])
+    const [todos , SetTodos] = useState([])
     const todoNameRef  = useRef()
+
+    useEffect(() => {
+        const storedTodo = JSON.parse(localStorage.getItem(LOCAL_STOREG_KEY))
+        if(storedTodo) SetTodos(storedTodo)
+    }, [])
+
+
+    useEffect(() => {
+        localStorage.setItem(LOCAL_STOREG_KEY, JSON.stringify(todos))
+    }, [todos])
+
+    function toggleTodo (id){
+        const newTodos = [...todos]
+        const todo = newTodos.find(todo => todo.id === id )
+        todo.complete = !todo.complete
+        SetTodos(newTodos)
+    }
+
     function handleAddTodo(e){
         const name = todoNameRef.current.value
         if( name === '') return
@@ -12,15 +40,34 @@ function App() {
         })
         todoNameRef.current.value = null        
     }
+    
+    function handleClearTodos(){
+        const newTodos = todos.filter(todo => !todo.complete)
+        SetTodos(newTodos)
+    }
+
     return (
         <>
+        
+            <div className="container">
+            <TodoList todos={todos} toggleTodo={toggleTodo}/>
+            <InputGroup>
+             <input className="col-md-4" placeholder="inter todos" ref={todoNameRef} type="text"/>
+            </InputGroup>
+            <ButtonGroup>
+            <Button outline color="success" onClick={handleAddTodo} > add Todo  </Button>
+            </ButtonGroup>
+            
             <div>
-            <TodoList todos={todos}/>
-            <input ref={todoNameRef} type="text"/>
-            <button onClick={handleAddTodo}> add Todo </button>
-            <button> clear </button>
-            <div> 0 todo </div>
+                 <Button outline color="warning" onClick={handleClearTodos}> clear </Button>
             </div>
+           
+            {/* <div> {todos.filter(todo=> !todo.complete).length} left to do </div> */}
+           <div className="col-md-3">
+            <Progress className="" color="danger" value={todos.filter(todo=> !todo.complete).length} left to do />
+            </div>
+            </div>
+            
         </>
     )
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from '../src/components/App';
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
